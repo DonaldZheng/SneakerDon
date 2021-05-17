1e3361752cf70f51661d23d374cb1004552811e6
DonaldZheng <dzheng112@gmail.com>
2021-05-17 14:58:30 -0500

    testing out my index/list page

--- theSneakerDon/Models/SneakerRepository.cs
+++ theSneakerDon/Models/SneakerRepository.cs
@@ -11,11 +11,11 @@ namespace theSneakerDon.Models
         public IEnumerable<Sneaker> GetAllSneaker => new List<Sneaker>
         {
              new Sneaker{ SneakerId = 1, Name =" Jordan 1", Price = 170.00M, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elite"
-                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://en.wikipedia.org/wiki/Sneakers#/media/File:Air_jordan_XI_space_jam.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://en.wikipedia.org/wiki/Nike,_Inc.#/media/File:Airjordan1985-1st.JPG" },
+                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="jordan4.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://sneakernews.com/wp-content/uploads/2019/02/travis-scott-jordan-1-CD4487-100-6.jpg" },
              new Sneaker{ SneakerId = 2, Name =" Jordan 3", Price = 170.00M, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elite"
-                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://en.wikipedia.org/wiki/Sneakers#/media/File:Air_jordan_XI_space_jam.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://en.wikipedia.org/wiki/Nike,_Inc.#/media/File:Airjordan1985-1st.JPG" },
+                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="jordan4.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://sneakernews.com/wp-content/uploads/2021/05/A-Ma-Maniere-Air-Jordan-3-DH3434-110-Global-Release-Info-4.jpg" },
              new Sneaker{ SneakerId = 3, Name =" Jordan 4", Price = 170.00M, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elite"
-                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://en.wikipedia.org/wiki/Sneakers#/media/File:Air_jordan_XI_space_jam.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://en.wikipedia.org/wiki/Nike,_Inc.#/media/File:Airjordan1985-1st.JPG" }
+                 , Category = _categoryRepository.GetAllCategories.ToList()[0], ImageUrl="jordan4.jpg", IsInStock=true, IsOnSale= false, ImageThumbnailUrl = "https://sneakernews.com/wp-content/uploads/2021/03/Air-Jordan-4-UNC-CT8527-400.jpg" }
         };
 
         public IEnumerable<Sneaker> GetSneakerOnSale => throw new NotImplementedException();
