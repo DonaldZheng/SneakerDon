70d47ea1993f9a9e8101a41d29cc4a8e60fd53ad
DonaldZheng <dzheng112@gmail.com>
2021-05-17 11:26:40 -0500

    added a sneaker class and a category class in the models

---
+++ theSneakerDon/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theSneakerDon.Models
+{
+    public class Category
+    {
+        public int CategoryId
+        {
+            get; set;
+        }
+        public int CategoryName
+        {
+            get; set;
+        }
+        public int CategoryDescription
+        {
+            get; set;
+        }
+        public List<Sneaker> Sneakers
+        {
+            get; set;
+        }
+    }
+}
